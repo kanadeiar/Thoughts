d28672818d506bd8d98c16358132fd289b6d2bdf
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-18 22:15:03 +0300

    добавление комментариев + добавление новых свойств

--- Common/Thoughts.Domain.Base/Entities/IUser.cs
+++ Common/Thoughts.Domain.Base/Entities/IUser.cs
@@ -2,10 +2,24 @@
 
 namespace Thoughts.Domain.Base.Entities
 {
+    /// <summary>Интерфейс сущности пользователя, который может оставлять записи</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
     public interface IUser<TKey> : IPerson<TKey>, INamedEntity<TKey>
     {
-        public int NotesCount { get; }
-        public IEnumerable<INote> Notes { get; set; }
+        /// <summary>Количество записей пользователя</summary>
+        public int BlogNotesCount { get; }
+
+        /// <summary>Количество коментариев пользователя</summary>
+        public int CommentsCount { get; }
+
+        /// <summary>Перечисление записей пользователя</summary>
+        /// /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+        public IEnumerable<INote<TKey>> BlogNotes { get; set; }
+
+        /// <summary>Перечисление комментариев пользователя</summary>
+        /// /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+        public IEnumerable<INote<TKey>> Comments { get; set; }
     }
+    /// <summary>Интерфейс сущности пользователя, который может оставлять записи</summary>
     public interface IUser : IUser<int>, IPerson, INamedEntity { }
 }
--- Common/Thoughts.Domain/Entities/User.cs
+++ Common/Thoughts.Domain/Entities/User.cs
@@ -1,19 +1,22 @@
 ﻿
-using System.Linq;
-
 using Thoughts.Domain.Base.Entities;
 
 namespace Thoughts.Domain.Entities
 {
+    /// <summary>Сущность пользователя, который может оставлять записи</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
     public class User<TKey> : IUser<TKey>
     {
-        public int NotesCount => Notes.Count();
-        public IEnumerable<INote> Notes { get; set; } = Enumerable.Empty<INote>();
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string Patronymic { get; set; }
         public string Name { get; set; }
         public TKey Id { get; set; }
+        public int BlogNotesCount => BlogNotes.Count();
+        public int CommentsCount => Comments.Count();
+        public IEnumerable<INote<TKey>> BlogNotes { get; set; } = Enumerable.Empty<INote<TKey>>();
+        public IEnumerable<INote<TKey>> Comments { get; set; } = Enumerable.Empty<INote<TKey>>();
     }
+    /// <summary>Сущность пользователя, который может оставлять записи</summary>
     public class User : User<int>, IUser { }
 }
