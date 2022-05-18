74f7ed84b38cf1b9bc0b3e94c30e63d592d606f1
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-18 21:28:23 +0300

    сущность записи и пользователя

---
+++ Common/Thoughts.Domain.Base/Entities/INote.cs
@@ -0,0 +1,14 @@
+﻿
+using Thoughts.Interfaces.Base.Entities;
+
+namespace Thoughts.Domain.Base.Entities
+{
+    public interface INote<TKey> : IEntity<TKey>
+    {
+        public IUser User { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
---
+++ Common/Thoughts.Domain.Base/Entities/IUser.cs
@@ -0,0 +1,10 @@
+﻿using Thoughts.Interfaces.Base.Entities;
+
+namespace Thoughts.Domain.Base.Entities
+{
+    public interface IUser : IPerson, INamedEntity
+    {
+        public string Nickname { get; set; }
+        public int NotesCount { get; set; }
+    }
+}
