f12e4f3fcc444118150ae66dc163e669578f760c
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-18 22:19:33 +0300

    добавление комментариев к интерфейсу и сущности категории

--- Common/Thoughts.Domain/Entities/Category.cs
+++ Common/Thoughts.Domain/Entities/Category.cs
@@ -2,10 +2,12 @@
 
 namespace Thoughts.Domain.Entities
 {
+    /// <summary>Сущность категории</summary>
     public class Category<TKey> : ICategory<TKey>
     {
         public string Name { get; set; }
         public TKey Id { get; set; }
     }
+    /// <summary>Сущность категории</summary>
     public class Category : Category<int>, ICategory { }
 }
