840a698183e9c413c5416f648f99fac2a9fbae63
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-18 22:21:48 +0300

    добавление комментариев к интерфейсу и сущности комментария

--- Common/Thoughts.Domain.Base/Entities/IComment.cs
+++ Common/Thoughts.Domain.Base/Entities/IComment.cs
@@ -1,8 +1,13 @@
 ﻿namespace Thoughts.Domain.Base.Entities
 {
+    /// <summary>Интерфейс сущности комментария</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
     public interface IComment<TKey> : INote<TKey>
     {
+        /// <summary>Перечисление ответов на коментарии</summary>
+        /// <typeparam name="TKey">Тип первичного ключа</typeparam>
         public IEnumerable<IComment<TKey>> Answers { get; set; }
     }
+    /// <summary>Интерфейс сущности комментария</summary>
     public interface IComment : IComment<int>, INote { }
 }
