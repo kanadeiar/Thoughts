bea955fba563b715954b1b86c398ff9e30afc896
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-18 21:54:09 +0300

    реализация сущности пользователя

---
+++ Common/Thoughts.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Linq;
+
+using Thoughts.Domain.Base.Entities;
+
+namespace Thoughts.Domain.Entities
+{
+    public class User<TKey> : IUser<TKey>
+    {
+        public int NotesCount => Notes.Count();
+        public IEnumerable<INote> Notes { get; set; } = Enumerable.Empty<INote>();
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public string Name { get; set; }
+        public TKey Id { get; set; }
+    }
+}
