bbf30eeea458eab86bb822a4a27963653b165013
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-18 22:05:55 +0300

    сущность записи блога

---
+++ Common/Thoughts.Domain/Entities/BlogNote.cs
@@ -0,0 +1,18 @@
+﻿using Thoughts.Domain.Base.Entities;
+
+namespace Thoughts.Domain.Entities
+{
+    public class BlogNote<TKey> : IBlogNote<TKey>
+    {
+        public string Title { get; set; }
+        public ICategory<TKey> Category { get; set; }
+        public IEnumerable<ITag<TKey>> Tags { get; set; }
+        public IEnumerable<IComment<TKey>> Comments { get; set; }
+        public IUser<TKey> Creator { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+        public TKey Id { get; set; }
+    }
+    public class BlogNote : BlogNote<int>, IBlogNote { }
+}
