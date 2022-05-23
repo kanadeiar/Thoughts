f9cebe9005fe3f8c15d7d4af8b1d14144f96e405
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-05-23 22:23:18 +0300

    Провайдер для PosgreSQL

---
+++ Data/Thoughts.DAL.PostgreSQL/Registrator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Thoughts.DAL.PostgreSQL
+{
+    public static class Registrator
+    {
+        public static IServiceCollection AddThoughtsDBPostgreSQL(this IServiceCollection services, string ConnectionString)
+        {
+            services.AddDbContext<ThoughtsDB>(opt => opt.UseNpgsql(ConnectionString, o => o.MigrationsAssembly(typeof(Registrator).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
