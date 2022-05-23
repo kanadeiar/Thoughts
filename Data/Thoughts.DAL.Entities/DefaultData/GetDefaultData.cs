2b06afca0ab5e6b8d3ebe653eb1e083ed4c19f1e
Георгий <georgij.tar@yandex.ru>
2022-05-23 21:13:39 +0300

    Первоначальная настройка WPF

--- Data/Thoughts.DAL.Entities/DefaultData/GetDefaultData.cs
+++ Data/Thoughts.DAL.Entities/DefaultData/GetDefaultData.cs
@@ -6,7 +6,8 @@ public static class GetDefaultData
     {
            new (){ Id=1, Name="Черновик"},
            new (){ Id=2, Name="Опубликовано"},
-           new (){ Id=3, Name="Зблакировано"}
+           new (){ Id=3, Name="На модерации"},
+           new (){ Id=4, Name="Зблакировано"}
     };
 
     public static Role[] DefaultRole() => new Role[]
@@ -14,5 +15,6 @@ public static class GetDefaultData
           new (){ Id=1, Name="Администратор"},
           new (){ Id=2, Name="Модератор"},
           new (){ Id=3, Name="Автор"},
+          new (){ Id=4, Name="Гость"},
     };
 }
