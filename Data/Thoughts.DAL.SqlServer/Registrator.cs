2159aa726b826b47966f7d54315af5ddc929c487
Андрей Демьянов <demyanovaf@vsk.sibur.ru>
2022-05-23 22:22:23 +0300

    Провайдер для SQLServer

---
+++ Data/Thoughts.DAL.SqlServer/Registrator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Thoughts.DAL.SqlServer
+{
+    public static class Registrator
+    {
+        public static IServiceCollection AddThoughtsDBSqlServer(this IServiceCollection services, string ConnectionString)
+        {
+            services.AddDbContext<ThoughtsDB>(opt => opt.UseSqlServer(ConnectionString, o => o.MigrationsAssembly(typeof(Registrator).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
