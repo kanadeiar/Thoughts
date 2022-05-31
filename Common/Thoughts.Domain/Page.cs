ff6dc3356ea59f35b60d6821a79568cfe542c2e6
Oswitto624 <dzirtsmd@mail.ru>
2022-06-01 02:07:42 +0300

    Модель страницы, реализует интерфейс IPage

---
+++ Common/Thoughts.Domain/Page.cs
@@ -0,0 +1,9 @@
+﻿using Thoughts.Interfaces.Base.Repositories;
+
+namespace Thoughts.Domain
+{
+    public record Page<T>(IEnumerable<T> Items, int PageNumber, int PageSize, int TotalCount) : IPage<T>
+    {
+        public int ItemsCount => (int)Math.Ceiling((double)TotalCount / PageSize);
+    }
+}
