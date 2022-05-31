11125a7c5b6a770dedc21d2105cff12ca2186e81
Oswitto624 <dzirtsmd@mail.ru>
2022-06-01 02:11:47 +0300

    Добавление внешних ключей для сущностей

--- Data/Thoughts.DAL.Entities/Category.cs
+++ Data/Thoughts.DAL.Entities/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +15,11 @@ public class Category : NamedEntity
 {
     /// <summary>Статус категории</summary>
     [Required]
+    [ForeignKey(nameof(StatusId))]
     public Status Status { get; set; } = null!;
+
+    /// <summary> Внешний ключ статуса категории </summary>
+    public int StatusId { get; set; }
 
     /// <summary>Список постов входящих в категорию</summary>
     public ICollection<Post> Posts { get; set; } = new HashSet<Post>();
