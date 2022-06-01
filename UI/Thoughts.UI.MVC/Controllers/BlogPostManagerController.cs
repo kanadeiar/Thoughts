d3d4435228616aa3c22cbd4d3162fd13d322ad24
Oswitto624 <dzirtsmd@mail.ru>
2022-06-01 17:27:29 +0300

    Index. Заготовка контроллера BlogPostManager

---
+++ UI/Thoughts.UI.MVC/Controllers/BlogPostManagerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Thoughts.Interfaces;
+
+namespace Thoughts.UI.MVC.Controllers;
+
+public class BlogPostManagerController : Controller 
+{
+    private readonly IBlogPostManager manager;
+    private readonly ILogger logger;
+
+    public BlogPostManagerController(IBlogPostManager Manager, ILogger Logger)
+    {
+        manager = Manager;
+        logger = Logger;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var posts = await manager.GetAllPostsAsync();
+        return View(posts);
+    }
+}
