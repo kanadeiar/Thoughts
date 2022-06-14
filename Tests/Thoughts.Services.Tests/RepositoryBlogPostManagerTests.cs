1b7b497aceeaff78ead09b46d09e69f1f72a825e
Oswitto624 <dzirtsmd@mail.ru>
2022-06-14 19:36:49 +0300

    Исправление инициализатора тестов, тесты для комплекта методов GetAll

--- Tests/Thoughts.Services.Tests/RepositoryBlogPostManagerTests.cs
+++ Tests/Thoughts.Services.Tests/RepositoryBlogPostManagerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 
+using Thoughts.Domain;
 using Thoughts.Domain.Base.Entities;
 using Thoughts.Interfaces;
 using Thoughts.Interfaces.Base.Repositories;
@@ -16,8 +17,8 @@ public class RepositoryBlogPostManagerTests
     private Mock<INamedRepository<Tag>> _Tag_Repo_Mock;
 
     private Category[] _Categories;
-    private Mock<INamedRepository<Category> > _Category_Repo_Mock;
-    
+    private Mock<INamedRepository<Category>> _Category_Repo_Mock;
+
     private Status[] _Statuses;
     private Mock<INamedRepository<Status>> _Status_Repo_Mock;
 
@@ -29,72 +30,16 @@ public class RepositoryBlogPostManagerTests
     [TestInitialize]
     public void TestInitialize()
     {
-        _Posts = new Post[]
-        {
-            new Post
-            {
-                Id = 1,
-                Title = "Title1",
-                Body = "Body1",
-                Tags = _Tags.Where(t => t.Id == 1).ToArray(),
-                Category = _Categories.First(c => c.Id == 1),
-                User = _Users.First(u => u.Id == "1"),
-            },
-            new()
-            {
-                Id = 2,
-                Title = "Title2",
-                Body = "Body2",
-                Tags = (ICollection<Tag>)_Tags.First(t => t.Id == 2),
-                Category = _Categories.First(c => c.Id == 1),
-                User = _Users.First(u => u.Id == "1"),
-            },
-            new()
-            {
-                Id = 3,
-                Title = "Title3",
-                Body = "Body3",
-                Tags = (ICollection<Tag>)_Tags.First(t => t.Id == 3),
-                Category = _Categories.First(c => c.Id == 1),
-                User = _Users.First(u => u.Id == "2"),
-            },
-            new()
-            {
-                Id = 4,
-                Title = "Title4",
-                Body = "Body4",
-                Tags = _Tags.Where(t => t.Id == 1).ToArray(),
-                Category = _Categories.First(c => c.Id == 2),
-                User = _Users.First(u => u.Id == "2"),
-            },
-            new()
-            {
-                Id = 5,
-                Title = "Title5",
-                Body = "Body5",
-                Tags = (ICollection<Tag>)_Tags.First(t => t.Id == 2),
-                Category = _Categories.First(c => c.Id == 3),
-                User = _Users.First(u => u.Id == "3"),
-            },
-            new()
-            {
-                Id = 6,
-                Title = "Title6",
-                Body = "Body6",
-                Tags = (ICollection<Tag>)_Tags.First(t => t.Id == 3),
-                Category = _Categories.First(c => c.Id == 1),
-                User = _Users.First(u => u.Id == "3"),
-            },
-        };
+
 
         _Tags = new Tag[]
         {
-                Id = 7,
-                Title = "Title7",
-                Body = "Body7",
-                Tags = new[] {_Tags[0], _Tags[2] },
-                Category = _Categories[1],
-                User = _Users.First(u => u.Id == "1"),
+            new(){ Id = 1, Name = "Tag1", },
+            new(){ Id = 2, Name = "Tag2", },
+            new(){ Id = 3, Name = "Tag3", },
+        };
+
+        _Categories = new Category[]
         {
             new(){ Id = 1, Name = "Category1", },
             new(){ Id = 2, Name = "Category2", },
@@ -127,25 +72,79 @@ public class RepositoryBlogPostManagerTests
             },
         };
 
+        _Posts = new Post[]
+        {
+            new Post
+            {
+                Id = 1,
+                Title = "Title1",
+                Body = "Body1",
+                Tags = _Tags.Where(t => t.Id == 1).ToArray(),
+                Category = _Categories.First(c => c.Id == 1),
+                User = _Users.First(u => u.Id == "1"),
+            },
+            new Post
+            {
+                Id = 2,
+                Title = "Title2",
+                Body = "Body2",
+                Tags = _Tags.Where(t => t.Id == 2).ToArray(),
+                Category = _Categories.First(c => c.Id == 1),
+                User = _Users.First(u => u.Id == "1"),
+            },
+            new Post
+            {
+                Id = 3,
+                Title = "Title3",
+                Body = "Body3",
+                Tags = _Tags.Where(t => t.Id == 3).ToArray(),
+                Category = _Categories.First(c => c.Id == 1),
+                User = _Users.First(u => u.Id == "2"),
+            },
+            new Post
+            {
+                Id = 4,
+                Title = "Title4",
+                Body = "Body4",
+                Tags = _Tags.Where(t => t.Id == 1).ToArray(),
+                Category = _Categories.First(c => c.Id == 2),
+                User = _Users.First(u => u.Id == "2"),
+            },
+            new Post
+            {
+                Id = 5,
+                Title = "Title5",
+                Body = "Body5",
+                Tags = _Tags.Where(t => t.Id == 2).ToArray(),
+                Category = _Categories.First(c => c.Id == 3),
+                User = _Users.First(u => u.Id == "3"),
+            },
+            new Post
+            {
+                Id = 6,
+                Title = "Title6",
+                Body = "Body6",
+                Tags = _Tags.Where(t => t.Id == 3).ToArray(),
+                Category = _Categories.First(c => c.Id == 1),
+                User = _Users.First(u => u.Id == "3"),
+            },
+            new Post
+            {
+                Id = 7,
+                Title = "Title7",
+                Body = "Body7",
+                Tags = new[] {_Tags[0], _Tags[2] },
+                Category = _Categories[1],
+                User = _Users.First(u => u.Id == "1"),
+            },
+        };
+
         _Post_Repo_Mock = new Mock<IRepository<Post>>();
-        _Post_Repo_Mock.Setup(p => p.GetAll(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(_Posts);
-
         _Tag_Repo_Mock = new Mock<INamedRepository<Tag>>();
-        _Tag_Repo_Mock.Setup(t => t.GetAll(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(_Tags);
-
         _Category_Repo_Mock = new Mock<INamedRepository<Category>>();
-        _Category_Repo_Mock.Setup(c => c.GetAll(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(_Categories);
-
         _Status_Repo_Mock = new Mock<INamedRepository<Status>>();
         _User_Repo_Mock = new Mock<IRepository<User, string>>();
-            .ReturnsAsync(_Statuses);
 
-        _User_Repo_Mock = new Mock<IRepository<User, string>>();
-        _User_Repo_Mock.Setup(u => u.GetAll(It.IsAny<CancellationToken>()))
-            .ReturnsAsync(_Users);
 
         _BlogPostManager = new RepositoryBlogPostManager(
             _Post_Repo_Mock.Object,
@@ -159,32 +158,94 @@ public class RepositoryBlogPostManagerTests
     [TestMethod]
     public async Task GetAllPostsAsync_Test_Returns_Posts_ToArray()
     {
-        Post[] expected_posts =
-        {
-            new() { Title = "Post1" },
-            new() { Title = "Post2" },
-            new() { Title = "Post3" },
-            new() { Title = "Post4" },
-        };
-
-        var post_repo_mock = new Mock<IRepository<Post>>();
-        post_repo_mock.Setup(c => c.GetAll(It.IsAny<CancellationToken>()))
+        var expected_posts = _Posts;
+        _Post_Repo_Mock.Setup(c => c.GetAll(It.IsAny<CancellationToken>()))
             .ReturnsAsync(expected_posts);
 
-        var tag_repo_mock = new Mock<INamedRepository<Tag>>();
-        var category_repo_mock = new Mock<INamedRepository<Category>>();
-        var statuses_repo_mock = new Mock<INamedRepository<Status>>();
-        var users_repo_mock = new Mock<IRepository<User, string>>();
-
-        var manager = new RepositoryBlogPostManager(
-            post_repo_mock.Object,
-            tag_repo_mock.Object,
-            category_repo_mock.Object,
-            statuses_repo_mock.Object,
-            users_repo_mock.Object);
-
-        var actual_posts = await manager.GetAllPostsAsync();
+        var actual_posts = await _BlogPostManager.GetAllPostsAsync();
 
         CollectionAssert.AreEqual(expected_posts, actual_posts.ToArray());
     }
+
+    [TestMethod]
+    public async Task GetAllPostsCountAsync_Test_Returns_CountOfAllPosts()
+    {
+        var posts = _Posts;
+        var expected_posts_count = posts.Count();
+
+        _Post_Repo_Mock.Setup(c => c.GetCount(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expected_posts_count);
+
+        var actual_posts_count = await _BlogPostManager.GetAllPostsCountAsync();
+
+        Assert.AreEqual(expected_posts_count, actual_posts_count);
+    }
+
+    [TestMethod]
+    public async Task GetAllPostsSkipTakeAsync_Test_Returns_EmptyEnumerable()
+    {
+        int skip = 2;
+        int take = 0; //проверяем If: Take == 0
+
+        var expected_page = _Posts.ToArray().Skip(skip).Take(take);
+
+        _Post_Repo_Mock.Setup(c => c.Get(skip, take, It.IsAny<CancellationToken>()));
+
+        var actual_page = await _BlogPostManager.GetAllPostsSkipTakeAsync(skip, take);
+
+        Assert.AreEqual(expected_page.Count(), actual_page.Count());
+    }
+
+    [TestMethod]
+    public async Task GetAllPostsSkipTakeAsync_Test_Returns_EnumPageOfPosts()
+    {
+        int skip = 2;
+        int take = 3;
+
+        var expected_page = _Posts.ToArray().Skip(skip).Take(take);
+
+        _Post_Repo_Mock.Setup(c => c.Get(skip, take, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expected_page);
+
+        var actual_page = await _BlogPostManager.GetAllPostsSkipTakeAsync(skip, take);
+
+        CollectionAssert.AreEqual(expected_page.ToArray(), actual_page.ToArray());
+    }
+
+    [TestMethod]
+    public async Task GetAllPostsPageAsync_Test_Returns_EmptyPage()
+    {
+        var total_count = _Posts.Count();
+
+        int pageIndex = 0;
+        int pageSize = 3;
+
+        var expected_page = new Page<Post>(Enumerable.Empty<Post>(), pageIndex, pageSize, total_count);
+
+        _Post_Repo_Mock.Setup(c => c.GetPage(pageIndex, pageSize, It.IsAny<CancellationToken>()));
+
+        var actual_page = await _BlogPostManager.GetAllPostsPageAsync(pageIndex, pageSize);
+
+        Assert.AreEqual(expected_page.Items, actual_page.Items);
+    }
+
+    [TestMethod]
+    public async Task GetAllPostsPageAsync_Test_Returns_Page()
+    {
+        var total_count = _Posts.Count();
+        int pageIndex = 3;
+        int pageSize = 3;
+        var posts = _Posts;
+
+        var expected_page = new Page<Post>(posts, pageIndex, pageSize, total_count);
+
+        _Post_Repo_Mock.Setup(c => c.GetPage(pageIndex, pageSize, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expected_page);
+
+        var actual_page = await _BlogPostManager.GetAllPostsPageAsync(pageIndex, pageSize);
+
+        Assert.AreEqual(expected_page, actual_page);
+    }
+
+
 }
