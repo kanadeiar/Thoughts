c53016753365ac7c18fa65d35b384561d615abe2
Infarh <shmachilin@yandex.ru>
2022-06-15 21:51:45 +0300

    Заготовка репозитория БД

---
+++ Data/Thoughts.DAL.Entities/Page.cs
@@ -0,0 +1,8 @@
+﻿using Thoughts.Interfaces.Base.Repositories;
+
+namespace Thoughts.DAL.Entities;
+
+public record Page<T>(IEnumerable<T> Items, int PageNumber, int PageSize, int TotalCount) : IPage<T>
+{
+    public int ItemsCount => (int)Math.Ceiling((double)TotalCount / PageSize);
+}
