e282ad72411b5ad418ae927c7aca294929eb4dff
Oswitto624 <dzirtsmd@mail.ru>
2022-06-21 20:48:28 +0300

    Избавление от мусорных кусков закомментированного кода

--- Tests/Thoughts.Services.Tests/InSQL/DbRepositoryTests.cs
+++ Tests/Thoughts.Services.Tests/InSQL/DbRepositoryTests.cs
@@ -17,7 +17,6 @@ public class DbRepositoryTests
 
     [TestInitialize]
     public async Task TestInitialize()
-    //public void TestInitialize()
     {
         _Logger_Mock = new Mock<ILogger<DbRepository<Tag>>>();
         
@@ -26,7 +25,6 @@ public class DbRepositoryTests
            .Options;
 
         var _db = new ThoughtsDB(_options);
-        //using var _db = new ThoughtsDB(_options);
 
         _tags = Enumerable.Range(1, 10)
            .Select(i => new Tag { Name = $"Tag-{i}" })
@@ -35,35 +33,15 @@ public class DbRepositoryTests
         await _db.Tags.AddRangeAsync(_tags);
         await _db.SaveChangesAsync();
 
-        //_db.AddRange(_tags);
-        //_db.SaveChanges();
-
         _tags_Repository = new DbRepository<Tag>(_db, _Logger_Mock.Object);
     }
 
     [TestMethod]
     public async Task Get_Returns_Items()
     {
-        //var logger_mock = new Mock<ILogger<DbRepository<Tag>>>();
-
-        //var options = new DbContextOptionsBuilder<ThoughtsDB>()
-        //   .UseInMemoryDatabase("TestDB")
-        //   .Options;
-
-        //await using var db = new ThoughtsDB(options);
-
-        //var tags = Enumerable.Range(1, 10)
-        //   .Select(i => new Tag { Name = $"Tag-{i}" })
-        //   .ToArray();
-        
         const int skip = 5;
         const int count = 2;
         var expected_tags = _tags.Skip(skip).Take(count).ToArray();
-
-        //_db.Tags.AddRange(tags);
-        //await _db.SaveChangesAsync();
-
-        //var tags_repository = new DbRepository<Tag>(db, logger_mock.Object);
 
         var actual_tags = await _tags_Repository.Get(skip, count);
 
