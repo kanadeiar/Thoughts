46247185995887cb578231cb1e6ddab439e94bc9
Infarh <shmachilin@yandex.ru>
2022-06-22 21:39:34 +0300

    Пример настройки хоста WebAPI

--- Tests/Thoughts.WebAPI.Tests.Integration/WeatherApiTests.cs
+++ Tests/Thoughts.WebAPI.Tests.Integration/WeatherApiTests.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
+using Thoughts.Interfaces.Base.Repositories;
+using Thoughts.Services.InSQL;
 using Thoughts.WebAPI.Clients.Test.Weather;
 
 namespace Thoughts.WebAPI.Tests.Integration;
@@ -7,12 +11,33 @@ namespace Thoughts.WebAPI.Tests.Integration;
 [TestClass]
 public class WeatherApiTests
 {
+    private WebApplicationFactory<Program> _WebAPIHostBuilder;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _WebAPIHostBuilder = new WebApplicationFactory<Program>()
+           //.WithWebHostBuilder(builder => builder
+           //    .ConfigureServices(services =>
+           //     {
+           //         services.RemoveAll(typeof(IRepository<>));
+           //         services.AddScoped(typeof(IRepository<>), typeof(DbRepository<>));
+           //     }))
+            ;
+    }
+
     [TestMethod]
     public async Task ReceiveDataFromService()
     {
-        var builder = new WebApplicationFactory<Program>();
 
-        var http_client = builder.CreateClient();
+        var http_client = _WebAPIHostBuilder
+           //.WithWebHostBuilder(builder => builder
+           //    .ConfigureServices(services =>
+           //     {
+           //         services.RemoveAll(typeof(IRepository<>));
+           //         services.AddScoped(typeof(IRepository<>), typeof(DbRepository<>));
+           //     }))
+           .CreateClient();
 
         var weather_client = new WeatherClient(http_client);
 
