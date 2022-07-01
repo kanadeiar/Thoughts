1f3310378b3d98fd04fb1ce37fe05b9d70cd2c72
kanadeiar <kanadeiar@gmail.com>
2022-07-01 11:41:16 +0300

    Создание и использование таг-хелпера

---
+++ UI/Thoughts.UI.MVC/TagHelpers/RawhtmlTagHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Thoughts.UI.MVC.TagHelpers;
+
+/// <summary>
+/// Таг-хелпер отображения html в сыром виде
+/// </summary>
+public class RawhtmlTagHelper : TagHelper
+{
+    private IHtmlHelper _htmlHelper { get; }
+    public RawhtmlTagHelper(IHtmlHelper htmlHelper)
+    {
+        _htmlHelper = htmlHelper ?? throw new ArgumentNullException(nameof(htmlHelper));
+    }
+
+    /// <summary>
+    /// Текст html
+    /// </summary>
+    public string Text { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = null;
+        var text = _htmlHelper.Raw(Text);
+        output.Content.SetHtmlContent(text);
+    }
+}
