e57207a336da800e5ec7b3d4f72e2f67db3e8ddb
gulyaev.eduard <gulyaev.ed.v@gmail.com>
2022-09-08 00:40:25 +0400

    добавил версионирование api swagger

--- Services/Thoughts.WebAPI/Program.cs
+++ Services/Thoughts.WebAPI/Program.cs
@@ -1,15 +1,74 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Reflection;
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+using Thoughts.DAL.Sqlite;
+using Thoughts.DAL.SqlServer;
+using Thoughts.Interfaces;
+using Thoughts.Services.InSQL;
+using Thoughts.WebAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+
+services.AddControllers();
+
+services.AddApiVersioning(opt =>
+{
+    opt.DefaultApiVersion = new ApiVersion(1, 0);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("x-api-version"),
+        new MediaTypeApiVersionReader("x-api-version"));
+});
+services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.ConfigureOptions<SwaggerConfigureOptions>();
+
+var db_type = configuration["Database"];
+
+switch (db_type)
+{
+    default: throw new InvalidOperationException($"Тип БД {db_type} не поддерживается");
+
+    case "Sqlite":
+        services.AddThoughtsDbSqlite(configuration.GetConnectionString("Sqlite"));
+        break;
+
+    case "SqlServer":
+        services.AddThoughtsDbSqlServer(configuration.GetConnectionString("SqlServer"));
+        break;
+}
+services.AddScoped<IBlogPostManager, SqlBlogPostManager>();
 
 var app = builder.Build();
-
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
 }
 
 app.UseHttpsRedirection();
