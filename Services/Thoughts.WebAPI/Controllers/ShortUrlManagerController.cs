260f161776901c2cbf94184ac265a505ef4476a4
Василий <vasyalvv@gmail.com>
2022-09-24 22:28:32 +0500

    WebApi контроллер сервиса коротких ссылок

---
+++ Services/Thoughts.WebAPI/Controllers/ShortUrlManagerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Thoughts.Interfaces.Base;
+
+namespace Thoughts.WebAPI.Controllers
+{
+    [Route("api/url")]
+    [ApiController]
+    public class ShortUrlManagerController : ControllerBase
+    {
+        private readonly IShortUrlManager _shortUrlManager;
+
+        public ShortUrlManagerController(IShortUrlManager ShortUrlManager)
+        {
+            _shortUrlManager = ShortUrlManager;
+        }
+
+          // GET: api/url?Alias=...
+        [HttpGet]
+        public Task<Uri?> GetUrl(string Alias) => _shortUrlManager.GetUrl(Alias);
+
+        // GET: api/url/10
+        [HttpGet("{Id}")]
+        public Task<Uri?> GetUrlById(int Id) => _shortUrlManager.GetUrlById(Id);
+
+        // PUT api/url
+        [HttpPut]
+        public Task<string> AddUrl( string Url)
+        {
+            _shortUrlManager.AddUrl(Url);
+        }
+
+        // DELETE api/url/10
+        [HttpDelete("{Id}")]
+        public Task<bool> DeleteUrl(int Id) => _shortUrlManager.DeleteUrl(Id);
+
+        // POST api/url/10
+        [HttpPost("{Id}")]
+        public Task<bool> UpdateUrl(int Id, [FromBody] string Url) => _shortUrlManager.UpdateUrl(Id, Url);
+    }
+}
