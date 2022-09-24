403c55a95f347ab7faf80e355ad66cd9e02e64ce
Василий <vasyalvv@gmail.com>
2022-09-24 17:49:32 +0500

    Сущность ссылки

---
+++ Data/Thoughts.DAL.Entities/ShortUrl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Thoughts.DAL.Entities.Base;
+
+namespace Thoughts.DAL.Entities
+{
+    public  class ShortUrl:Entity
+    {
+        /// <summary> Оригинальный URL </summary>
+        [Required]
+        public Uri OriginalUrl { get; set; }
+
+        /// <summary> Псевдоним ссылки </summary>
+        [Required]
+        public string Alias { get; set; }
+    }
+}
