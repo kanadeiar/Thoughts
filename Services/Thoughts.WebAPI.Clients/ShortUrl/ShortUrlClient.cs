774f694cbd0e3660ceaffca1e22ca27b334aa28a
Василий <vasyalvv@gmail.com>
2022-09-25 00:37:55 +0500

    WebApi клиент сервиса коротких ссылок

---
+++ Services/Thoughts.WebAPI.Clients/ShortUrl/ShortUrlClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+
+using Thoughts.Interfaces.Base;
+using Thoughts.WebAPI.Clients.Base;
+
+namespace Thoughts.WebAPI.Clients.ShortUrl
+{
+    public class ShortUrlClient: BaseClient, IShortUrlManager
+    {
+        public ShortUrlClient(IConfiguration Configuration):base(Configuration,WebApiControllersPath.ShortUrl)
+        {
+
+        }
+
+        public async Task<string> AddUrlAsync(string Url, CancellationToken Cancel = default)
+        {
+            var response = await PutAsync<string>($"{WebApiControllersPath.ShortUrl}?Url={Url}", "");
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<bool> DeleteUrlAsync(int Id, CancellationToken Cancel = default)
+        {
+            var response = await DeleteAsync($"{WebApiControllersPath.ShortUrl}/{Id}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<Uri?> GetUrlAsync(string Alias, CancellationToken Cancel = default)
+        {
+            var response = await GetAsync<Uri>($"{WebApiControllersPath.ShortUrl}?Alias={Alias}");
+            return response;
+        }
+
+        public async Task<Uri?> GetUrlByIdAsync(int Id, CancellationToken Cancel = default)
+        {
+            var response = await GetAsync<Uri>($"{WebApiControllersPath.ShortUrl}/{Id}");
+            return response;
+        }
+
+        public async Task<bool> UpdateUrlAsync(int Id, string Url, CancellationToken Cancel = default)
+        {
+            var response = await PutAsync<string>($"{WebApiControllersPath.ShortUrl}/{Id}", Url);
+            return await response.Content.ReadAsAsync<bool>();
+        }
+    }
+}
