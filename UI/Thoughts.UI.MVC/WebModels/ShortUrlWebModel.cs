43f854aeb0fc4acf3cc8157661739222d976de0f
Василий <vasyalvv@gmail.com>
2022-09-25 15:59:12 +0500

    В основное веб-приложение добавлены элементы ShortUrl: - Контроллер - Представления - Модель-представление

---
+++ UI/Thoughts.UI.MVC/WebModels/ShortUrlWebModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Thoughts.UI.MVC.WebModels
+{
+    public class ShortUrlWebModel
+    {
+        public string OriginalUrl { get; set; }
+        public string Alias { get; set; }   
+    }
+}
