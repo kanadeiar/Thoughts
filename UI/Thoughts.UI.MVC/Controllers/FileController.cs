45be327c3de5eca61de947810ed879ecb407bf18
Infarh <shmachilin@yandex.ru>
2022-10-05 19:48:07 +0300

    Заготовка для файлового менеджера в WEB

---
+++ UI/Thoughts.UI.MVC/Controllers/FileController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thoughts.UI.MVC.Controllers;
+public class FileController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
