c884a001eb179057cde0b7eec95a9345556e8608
Infarh <shmachilin@yandex.ru>
2022-10-12 20:53:47 +0300

    Заготовка для тестирования авторизации через WebAPI для HttpClient

--- Tests/Thoughts.TestConsole/Program.cs
+++ Tests/Thoughts.TestConsole/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.Extensions.Logging;
+
+using Thoughts.WebAPI.Clients.Identity;
+
+var http = new HttpClient
+{
+    BaseAddress = new("https://localhost:5011")
+};
+
+//ILogger<AccountClient> client_logger = LoggerFactory.Create(opt => opt.Add)
+
+var account_client = new AccountClient(http);
+
+Console.WriteLine("Ожидание сервера");
+Console.ReadLine();
+
+// Реализация авторизации, получение JWT от сервиса, установка заголовка в http
+//http.DefaultRequestHeaders.Add("???", ???);
+
+var users = await account_client.GetAllUsersAsync();
+
+Console.WriteLine("End.");
