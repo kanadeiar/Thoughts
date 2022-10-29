d46008df9a99ed1f4181c75f5a8491d194915a7c
Artem Britvin <a.britvin@retailrobotics.onmicrosoft.com>
2022-10-29 21:10:46 +0300

    created interface for file manage

---
+++ UI/Thoughts.UI.MAUI/Services/FileManager.cs
@@ -0,0 +1,14 @@
+﻿using Thoughts.UI.MAUI.Services.Interfaces;
+
+namespace Thoughts.UI.MAUI.Services
+{
+    public class FileManager : IFileManager
+    {
+        public async Task<bool> UploadFileAsync(FileResult file, CancellationToken token = default)
+        {
+            return await Task.FromResult(true);
+        }
+
+        public bool UploadFile(FileResult file) => UploadFileAsync(file).GetAwaiter().GetResult();
+    }
+}
---
+++ UI/Thoughts.UI.MAUI/Services/Interfaces/IFileManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Thoughts.UI.MAUI.Services.Interfaces
+{
+    public interface IFileManager
+    {
+        Task<bool> UploadFileAsync(FileResult file, CancellationToken token = default);
+
+        bool UploadFile(FileResult file);
+    }
+}
