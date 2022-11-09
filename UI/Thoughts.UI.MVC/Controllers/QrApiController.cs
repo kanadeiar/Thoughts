dcd5a74f616b3409ffcbb9ffdb579e05dc49f011
gulyaev.ed <gulyaev.ed.v@gmail.com>
2022-11-09 18:21:01 +0400

    Подобрать NuGet инструменты для генерации QR-кодов в виде картинок #99

---
+++ UI/Thoughts.UI.MVC/Controllers/QrApiController.cs
@@ -0,0 +1,27 @@
+﻿using QRCoder;
+
+namespace Thoughts.UI.MVC.Controllers
+{
+    public class QrApiController : ControllerBase
+    {
+        private readonly ILogger<QrApiController> _logger;
+        public QrApiController(ILogger<QrApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Get(string code)
+        {
+            if (string.IsNullOrEmpty(code)) 
+                return NotFound();
+
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(code,
+                QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrCodeData);
+            var result = qrCode.GetGraphic(10);
+
+            return File(result, "application/png", "qrcode.png");
+        }
+    }
+}
